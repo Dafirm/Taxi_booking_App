6cb096a1af37a7e709a3025ff38e7645bc720065
dafirm <ayanfeoyeniyi@gmail.com>
2023-08-24 07:51:22 +0300

    setting up

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from '@clerk/nextjs';
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+    
+      <h2>Subscribe here</h2>
+    </div>
+  );
+}
